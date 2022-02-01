c06ba9141f26cfd0f24ebcd2f3d192e95b937dce
Cyinthia <Cyinthia@DESKTOP-N8ID8VL>
2022-01-31 18:00:58 -0600

    Creacion del controlador Modelo y Producto "Pizza de 6 Quesos".

---
+++ QuieroPizza/QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -0,0 +1,22 @@
+﻿using QuieroPizza.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuieroPizza.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto = new ProductoModel();
+            producto.Id = 1;
+            producto.Descripcion = "Pizza de 6 quesos.";
+
+            return View(producto);
+        }
+    }
+}
