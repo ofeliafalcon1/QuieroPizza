65fff55a88b9fa06b22a48f0b4198f5b27027674
Cyinthia <Cyinthia@DESKTOP-N8ID8VL>
2022-01-31 18:58:51 -0600

    Creando una lista de productos de diferentes Pizzas.

--- QuieroPizza/QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
+++ QuieroPizza/QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -12,11 +12,29 @@ namespace QuieroPizza.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto = new ProductoModel();
-            producto.Id = 1;
-            producto.Descripcion = "Pizza de 6 quesos.";
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Pizza de 6 quesos.";
 
-            return View(producto);
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Pizza de 4 estaciones.";
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Pizza de jamon y queso.";
+
+            var producto4 = new ProductoModel();
+            producto4.Id = 4;
+            producto4.Descripcion = "Pizza de Peperoni y queso.";
+
+            var ListadeProductos = new List<ProductoModel>();
+            ListadeProductos.Add(producto1);
+            ListadeProductos.Add(producto2);
+            ListadeProductos.Add(producto3);
+            ListadeProductos.Add(producto4);
+
+            return View(ListadeProductos);
         }
     }
 }
